d7df78f6122beebf217c5d887324eb3ce7451567
bluefaceb4by37 <kochamalternatywki@ogarnij.se>
2025-05-09 17:37:03 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const songName = document.getElementById('song-name');
+    
+    // Przykład zmiany tytułu utworu po załadowaniu strony
+    songName.textContent = "Tytuł utworu z SoundCloud";
+});
